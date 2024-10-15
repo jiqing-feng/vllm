18ef2e74b3d0382db4f8276c05e4705b6483c64a
jiqing-feng <jiqing.feng@intel.com>
2024-10-15 09:11:13 -0400

    new scheduler

--- vllm/spec_decode/spec_decode_worker.py
+++ vllm/spec_decode/spec_decode_worker.py
@@ -41,6 +41,8 @@ from vllm.worker.worker_base import LoraNotSupportedWorkerBase, WorkerBase
 
 from concurrent.futures import ThreadPoolExecutor
 
+from vllm.engine.llm_engine import SchedulerOutputState
+
 logger = init_logger(__name__)
 
 
@@ -452,6 +454,125 @@ class SpecDecodeWorker(LoraNotSupportedWorkerBase):
         return self._run_speculative_decoding_step(execute_model_req,
                                                    num_lookahead_slots)
 
+    def get_seq(self, llm_engine, virtual_engine):
+        cached_outputs = llm_engine.cached_scheduler_outputs[virtual_engine]
+        seq_group_metadata_list = cached_outputs.seq_group_metadata_list
+        scheduler_outputs = cached_outputs.scheduler_outputs
+        allow_async_output_proc = cached_outputs.allow_async_output_proc
+
+        ctx = llm_engine.scheduler_contexts[virtual_engine]
+
+        # Clear outputs for each new scheduler iteration
+        ctx.request_outputs.clear()
+
+        # Skip the scheduler if there are any remaining steps in the seq groups.
+        # This ensures that the scheduler is only called again when the current
+        # batch has completed.
+        if not llm_engine._has_remaining_steps(seq_group_metadata_list):
+            # Schedule iteration
+            (seq_group_metadata_list, scheduler_outputs,
+             allow_async_output_proc
+             ) = llm_engine.scheduler[virtual_engine].schedule()
+
+            ctx.seq_group_metadata_list = seq_group_metadata_list
+            ctx.scheduler_outputs = scheduler_outputs
+
+            # Maybe switch from async mode to sync mode
+            if not allow_async_output_proc and len(ctx.output_queue) > 0:
+                llm_engine._process_model_outputs(ctx=ctx)
+
+            if (llm_engine.scheduler_config.is_multi_step
+                    and scheduler_outputs.num_lookahead_slots > 0):
+                # cache the scheduler outputs for the next iteration if we have
+                # lookahead slots
+                llm_engine._cache_scheduler_outputs_for_multi_step(
+                    virtual_engine, seq_group_metadata_list, scheduler_outputs,
+                    allow_async_output_proc)
+
+        assert seq_group_metadata_list is not None
+        assert scheduler_outputs is not None
+
+        if not scheduler_outputs.is_empty():
+            finished_requests_ids = llm_engine.scheduler[
+                virtual_engine].get_and_reset_finished_requests_ids()
+
+            # Check if we have a cached last_output from the previous iteration.
+            # For supporting PP this is probably the best way to pass the
+            # sampled_token_ids, as a separate broadcast over all the PP stages
+            # will cause one virtual engine's microbatch to block the pipeline.
+            last_sampled_token_ids = \
+                llm_engine._get_last_sampled_token_ids(virtual_engine)
+
+            execute_model_req = ExecuteModelRequest(
+                seq_group_metadata_list=seq_group_metadata_list,
+                blocks_to_swap_in=scheduler_outputs.blocks_to_swap_in,
+                blocks_to_swap_out=scheduler_outputs.blocks_to_swap_out,
+                blocks_to_copy=scheduler_outputs.blocks_to_copy,
+                num_lookahead_slots=scheduler_outputs.num_lookahead_slots,
+                running_queue_size=scheduler_outputs.running_queue_size,
+                finished_requests_ids=finished_requests_ids,
+                # We use ExecuteModelRequest to pass the last sampled_token_ids
+                # to each of the non-last PP stages for in-place prepare_input.
+                last_sampled_token_ids=last_sampled_token_ids)
+
+            return ctx, execute_model_req, scheduler_outputs, seq_group_metadata_list, allow_async_output_proc
+        else:
+            return ctx, None, scheduler_outputs, seq_group_metadata_list, allow_async_output_proc
+
+    def post_process(self, llm_engine, outputs, ctx, scheduler_outputs, seq_group_metadata_list, allow_async_output_proc):
+        # Finish the current step for all the sequence groups.
+        if llm_engine.scheduler_config.is_multi_step:
+            for seq_group in seq_group_metadata_list:
+                seq_group.finish_step()
+
+        if not llm_engine._has_remaining_steps(seq_group_metadata_list):
+            # clear the cache if we have finished all the steps.
+            if llm_engine.scheduler_config.is_multi_step:
+                llm_engine.cached_scheduler_outputs[0] = SchedulerOutputState()
+
+            # Add results to the output_queue
+            ctx.append_output(outputs=outputs,
+                              seq_group_metadata_list=seq_group_metadata_list,
+                              scheduler_outputs=scheduler_outputs,
+                              is_async=allow_async_output_proc,
+                              is_last_step=True)
+
+            if outputs and allow_async_output_proc:
+                assert len(outputs) == 1, (
+                    "Async postprocessor expects only a single output set")
+
+                llm_engine._advance_to_next_step(
+                    outputs[0], seq_group_metadata_list,
+                    scheduler_outputs.scheduled_seq_groups)
+
+            # Check if need to run the usual non-async path
+            if not allow_async_output_proc:
+                llm_engine._process_model_outputs(ctx=ctx)
+
+                # Log stats.
+                llm_engine.do_log_stats(scheduler_outputs, outputs)
+
+                # Tracing
+                llm_engine.do_tracing(scheduler_outputs)
+        else:
+            # Multi-step case
+            return ctx.request_outputs
+
+        if not llm_engine.has_unfinished_requests():
+            # Drain async postprocessor (if exists)
+            if len(ctx.output_queue) > 0:
+                llm_engine._process_model_outputs(ctx=ctx)
+            assert len(ctx.output_queue) == 0
+
+            # Stop the execute model loop in parallel workers until there are
+            # more requests to process. This avoids waiting indefinitely in
+            # torch.distributed ops which may otherwise timeout, and unblocks
+            # the RPC thread in the workers so that they can process any other
+            # queued control plane messages, such as add/remove lora adapters.
+            llm_engine.model_executor.stop_remote_worker_execution_loop()
+
+        return ctx.request_outputs
+
     @torch.inference_mode()
     def execute_model_hete_spec_decode(self, llm_engine):
         # Run the engine.
@@ -463,7 +584,7 @@ class SpecDecodeWorker(LoraNotSupportedWorkerBase):
             postfix=(f"est. speed input: {0:.2f} toks/s, "
                         f"output: {0:.2f} toks/s"),
         )
-        outputs: List[Union[RequestOutput, EmbeddingRequestOutput]] = []
+        outputs = []
         total_in_toks = 0
         total_out_toks = 0
 
@@ -476,21 +597,7 @@ class SpecDecodeWorker(LoraNotSupportedWorkerBase):
         sub_thread.result()
         # import time
         while llm_engine.has_unfinished_requests():
-            seq_group_metadata_list, scheduler_outputs = llm_engine.scheduler[0].schedule()
-            finished_requests_ids = llm_engine.scheduler[0].get_and_reset_finished_requests_ids()
-
-            execute_model_req = None
-            run_spec = None
-            if not scheduler_outputs.is_empty():
-                execute_model_req = ExecuteModelRequest(
-                    virtual_engine=0,
-                    seq_group_metadata_list=seq_group_metadata_list,
-                    blocks_to_swap_in=scheduler_outputs.blocks_to_swap_in,
-                    blocks_to_swap_out=scheduler_outputs.blocks_to_swap_out,
-                    blocks_to_copy=scheduler_outputs.blocks_to_copy,
-                    num_lookahead_slots=scheduler_outputs.num_lookahead_slots,
-                    running_queue_size=scheduler_outputs.running_queue_size,
-                    finished_requests_ids=finished_requests_ids)
+            ctx, execute_model_req, scheduler_outputs, seq_group_metadata_list, allow_async_output_proc = self.get_seq(llm_engine, 0)
 
             if execute_model_req:
                 run_spec = self.get_if_run_no_spec(execute_model_req)
@@ -498,12 +605,15 @@ class SpecDecodeWorker(LoraNotSupportedWorkerBase):
                     total_in_toks, total_out_toks = self.update_pbar(request_outputs, total_in_toks, total_out_toks, outputs, pbar)
                     total_in_toks, total_out_toks = self.update_pbar(request_outputs_2, total_in_toks, total_out_toks, outputs, pbar)
                     output = self._run_no_spec(execute_model_req, skip_proposer=run_spec, pool=pool)
-                    request_outputs = llm_engine._process_model_outputs(
-                        output, scheduler_outputs.scheduled_seq_groups,
-                        scheduler_outputs.ignored_seq_groups, seq_group_metadata_list)
-
-                    llm_engine.do_log_stats(scheduler_outputs, output)
-                    llm_engine.do_tracing(scheduler_outputs)
+                    request_outputs = self.post_process(llm_engine, output, ctx, scheduler_outputs, seq_group_metadata_list, allow_async_output_proc)
+            else:
+                # Nothing scheduled => If there is pending async postprocessor,
+                # then finish it here.
+                if len(ctx.output_queue) > 0:
+                    llm_engine._process_model_outputs(ctx=ctx)
+                # No outputs in this case
+                outputs = []
+                request_outputs = self.post_process(llm_engine, output, ctx, scheduler_outputs, seq_group_metadata_list, allow_async_output_proc)
 
             if execute_model_req and run_spec == "run_spec":
                 # Pass last hidden states from target model to proposer
@@ -539,40 +649,26 @@ class SpecDecodeWorker(LoraNotSupportedWorkerBase):
                     k=execute_model_req_2.num_lookahead_slots,
                     stage_times=stage_times,)
 
-                request_outputs_2 = llm_engine._process_model_outputs(
-                    output_2, scheduler_outputs_2.scheduled_seq_groups,
-                    scheduler_outputs_2.ignored_seq_groups, seq_group_metadata_list_2)
-
-                llm_engine.do_log_stats(scheduler_outputs_2, output_2)
-                llm_engine.do_tracing(scheduler_outputs_2)
+                request_outputs_2 = self.post_process(llm_engine, output_2, ctx_2, scheduler_outputs_2, seq_group_metadata_list_2, allow_async_output_proc_2)
 
             total_in_toks, total_out_toks = self.update_pbar(request_outputs_2, total_in_toks, total_out_toks, outputs, pbar)
-            seq_group_metadata_list_2, scheduler_outputs_2 = llm_engine.scheduler[1].schedule()
-            finished_requests_ids_2 = llm_engine.scheduler[1].get_and_reset_finished_requests_ids()
-            execute_model_req_2 = None
-            run_spec_2 = None
-            if not scheduler_outputs_2.is_empty():
-                execute_model_req_2 = ExecuteModelRequest(
-                    virtual_engine=1,
-                    seq_group_metadata_list=seq_group_metadata_list_2,
-                    blocks_to_swap_in=scheduler_outputs_2.blocks_to_swap_in,
-                    blocks_to_swap_out=scheduler_outputs_2.blocks_to_swap_out,
-                    blocks_to_copy=scheduler_outputs_2.blocks_to_copy,
-                    num_lookahead_slots=scheduler_outputs_2.num_lookahead_slots,
-                    running_queue_size=scheduler_outputs_2.running_queue_size,
-                    finished_requests_ids=finished_requests_ids_2)
+            ctx_2, execute_model_req_2, scheduler_outputs_2, seq_group_metadata_list_2, allow_async_output_proc_2 = self.get_seq(llm_engine, 0)
             if execute_model_req_2:
                 run_spec_2 = self.get_if_run_no_spec(execute_model_req_2)
                 if run_spec_2 != "run_spec":
                     total_in_toks, total_out_toks = self.update_pbar(request_outputs, total_in_toks, total_out_toks, outputs, pbar)
                     total_in_toks, total_out_toks = self.update_pbar(request_outputs_2, total_in_toks, total_out_toks, outputs, pbar)
                     output_2 = self._run_no_spec(execute_model_req_2, skip_proposer=run_spec_2, pool=pool)
-                    request_outputs_2 = llm_engine._process_model_outputs(
-                        output_2, scheduler_outputs_2.scheduled_seq_groups,
-                        scheduler_outputs_2.ignored_seq_groups, seq_group_metadata_list_2)
+                    request_outputs_2 = self.post_process(llm_engine, output_2, ctx_2, scheduler_outputs_2, seq_group_metadata_list_2, allow_async_output_proc_2)
+            else:
+                # Nothing scheduled => If there is pending async postprocessor,
+                # then finish it here.
+                if len(ctx.output_queue) > 0:
+                    llm_engine._process_model_outputs(ctx=ctx)
+                # No outputs in this case
+                output_2 = []
+                request_outputs_2 = self.post_process(llm_engine, output_2, ctx_2, scheduler_outputs_2, seq_group_metadata_list_2, allow_async_output_proc_2)
 
-                    llm_engine.do_log_stats(scheduler_outputs_2, output_2)
-                    llm_engine.do_tracing(scheduler_outputs_2)
             if execute_model_req_2 and run_spec_2 == "run_spec":
                 # Pass last hidden states from target model to proposer
                 execute_model_req_2.previous_hidden_states = self.previous_hidden_states_2
@@ -609,12 +705,10 @@ class SpecDecodeWorker(LoraNotSupportedWorkerBase):
                     k=execute_model_req.num_lookahead_slots,
                     stage_times=stage_times,)
 
-                request_outputs = llm_engine._process_model_outputs(
-                    output, scheduler_outputs.scheduled_seq_groups,
-                    scheduler_outputs.ignored_seq_groups, seq_group_metadata_list)
-
-                llm_engine.do_log_stats(scheduler_outputs, output)
-                llm_engine.do_tracing(scheduler_outputs)
+                request_outputs = self.post_process(llm_engine, output, ctx, scheduler_outputs, seq_group_metadata_list, allow_async_output_proc)
+            else:
+                if len(ctx.output_queue) > 0:
+                    llm_engine._process_model_outputs(ctx=ctx)
 
             total_in_toks, total_out_toks = self.update_pbar(request_outputs, total_in_toks, total_out_toks, outputs, pbar)
             if execute_model_req_2 and run_spec_2 == "run_spec":
@@ -623,7 +717,6 @@ class SpecDecodeWorker(LoraNotSupportedWorkerBase):
         total_in_toks, total_out_toks = self.update_pbar(request_outputs, total_in_toks, total_out_toks, outputs, pbar)
         total_in_toks, total_out_toks = self.update_pbar(request_outputs_2, total_in_toks, total_out_toks, outputs, pbar)
         pbar.close()
-
         pool.shutdown()
 
         return outputs
